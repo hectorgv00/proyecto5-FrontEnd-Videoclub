67598eb867a46e4329ac9c95bbc98e5656e1bd70
Diego.W <Di3LuX_11@hotmail.es>
2022-12-01 01:43:39 +0100

    trying to fix validation user on delete

--- src/containers/ProfileModify/ProfileModify.jsx
+++ src/containers/ProfileModify/ProfileModify.jsx
@@ -1,43 +1,39 @@
 import React, { useState, useEffect } from "react";
-import { Input } from 'antd';
-import { errorCheck } from '../../services/useful';
-import "./ProfileModify.css"
-import Button from "../../components/Button/Button";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { login } from "../../slices/userSlice";
 import { useJwt } from "react-jwt";
+import { Input } from 'antd';
+import { useDispatch } from "react-redux";
+import { errorCheck } from '../../services/useful';
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import "./ProfileModify.css"
 
-import { useDispatch, useSelector } from "react-redux";
-import { userData, userout, login } from "../../slices/userSlice";
 
+function ProfileModify() {
 
+  const dispatch = useDispatch();
 
-
-function ProfileModify(props) {
-
-  const dispatch = useDispatch()
-
-  let localStorageToken = localStorage.getItem("jwt")
+  let localStorageToken = localStorage.getItem("jwt");
   let { decodedToken } = useJwt(localStorageToken);
 
   const [user, setUser] = useState({
     name: "",
     surname: "",
     address: "",
-
-  })
+  });
 
   const [userError, setUserError] = useState({
     nameError: "",
     surnameError: "",
     addressError: "",
     nocompletedError: "",
-
-  })
+  });
 
   if (decodedToken === null) {
     decodedToken = { email: "" };
-  }
+  };
+
   const body = {
     name: user.name,
     surname: user.surname,
@@ -56,65 +52,55 @@ function ProfileModify(props) {
     } else {
       setUserError(((prevState) => ({
         ...prevState,
-        nocompletedError: "No se puede enviar el formulario. Revise que no hay campos vacíos y que el formato de los mismos es el correcto"
+        nocompletedError: "Necesitas introducir nuevos datos para que sean modificados"
       })));
     }
   };
 
   let validateInputs = (body) => {
     if ((body.name !== "" && body.surname !== "" && body.address !== "") && (userError.nameError === "" && userError.surnameError === "" && userError.addressError === "")) { return true }
-  }
+  };
+
   const userUpdater = async (body) => {
     let config = {
       headers: { Authorization: "Bearer " + localStorageToken }
     }
-    let resp = await axios.put("http://127.0.0.1:3000/users/modify", body, config)
-    let jwt = resp.data.jwt;
+    let res = await axios.put("http://127.0.0.1:3000/users/modify", body, config)
+    let jwt = res.data.jwt;
     let credentials = {
       token: jwt
     }
-    dispatch(userout({ credentials: {} }));
-    dispatch(login({ credentials: credentials }));
-
-    localStorage.removeItem("jwt");
+    // dispatch(userout({ credentials: {} }))
+    dispatch(login({ credentials: credentials }))
+    // localStorage.removeItem("jwt")
     localStorage.setItem("jwt", credentials.token)
     navigate("/")
-  }
+  };
 
   const navigate = useNavigate();
 
-
-
-  // Handler de los inputs
-
   const inputHandler = (e) => {
-
     setUser((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value
-
     }));
-  }
+  };
 
   const errorHandler = (field, value, type, password1) => {
-
     let error = "";
-
     error = errorCheck(value, type, password1);
-
     setUserError(((prevState) => ({
       ...prevState,
       [field + "Error"]: error
-
     })));
+  };
 
-    
-  }
   useEffect(() => {
     if (localStorage.getItem("jwt") === null) {
-        navigate("/");
+      navigate("/");
     };
-});
+  });
+
 
   return (
     <form onSubmit={(e) => updateUser(e)} className="container-fluid bg-black vh-100 d-flex justify-content-center align-items-center mt-5 mt-lg-0">
@@ -150,6 +136,6 @@ function ProfileModify(props) {
       </div>
     </form>
   );
-}
+};
 
 export default ProfileModify;
